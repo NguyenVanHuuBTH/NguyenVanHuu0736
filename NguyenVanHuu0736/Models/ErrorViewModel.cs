48d34ee0c6e49e5a89c0794e87b473dddd133dfe
Nguyễn Văn HữuBTH <NguyenVanHuuBTH@github.com>
2023-04-24 15:11:38 +0700

    NVH0736

---
+++ NguyenVanHuu0736/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenVanHuu0736.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
